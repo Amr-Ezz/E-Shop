3bb3dc576a06acfa45e995b25c44b8264e049c10
Ezz <amroelghitany@gmail.com>
2024-08-20 01:48:51 +0300

    CartCounter

--- src/components/Modal/CartModal.tsx
+++ src/components/Modal/CartModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { useCart } from "../../Context/CartContext";
 
@@ -60,7 +60,7 @@ const CartItem = styled.div`
       text-align: left;
       padding-top: 10px;
       span {
-      color: green;
+        color: green;
       }
     }
   }
@@ -80,6 +80,35 @@ const CartItem = styled.div`
       color: ${(props) => props.theme.colors.white};
     }
   }
+`;
+const CounterButton = styled.button`
+  background-color: #f0f0f0;
+  border: none;
+  border-radius: 50%;
+  width: 32px;
+  height: 32px;
+  font-size: 1.2rem;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:hover {
+    background-color: #e0e0e0;
+  }
+`;
+
+const CounterText = styled.p`
+  margin: 0 16px;
+  font-size: 1.5rem;
+`;
+const Counter = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 24px;
+`;
+const CounterWrapper = styled.div`
+  padding: 1rem;
 `;
 
 interface CartModalProps {
@@ -102,13 +131,32 @@ const CartModal: React.FC<CartModalProps> = ({ onClose }) => {
             />
             <div>
               <h3>{item.title}</h3>
-              <p>Price: <span>$</span>{item.price}</p>
-              {/* <input
-                type="number"
-                min="1"
-                value={item.quantity}
-                onChange={e => updateQuantity(item.id, parseInt(e.target.value))}
-              /> */}
+              <p>
+                Price: <span>$</span>
+                {item.price * item.quantity}
+              </p>
+
+              <CounterWrapper>
+                <Counter>
+                  <CounterButton
+                    onClick={() => {
+                      const updatedCounter = Math.max(1, item.quantity - 1);
+                      updateQuantity(item.id, updatedCounter);
+                    }}
+                  >
+                    -
+                  </CounterButton>
+                  <CounterText>{item.quantity}</CounterText>
+                  <CounterButton
+                    onClick={() => {
+                      const updatedCounter = Math.max(1, item.quantity + 1);
+                      updateQuantity(item.id, updatedCounter);
+                    }}
+                  >
+                    +
+                  </CounterButton>
+                </Counter>
+              </CounterWrapper>
             </div>
             <button onClick={() => removeFromCart(item.id)}>Remove</button>
           </CartItem>
